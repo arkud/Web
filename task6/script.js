c2ed416f0f0a55a52194fa03bedc50cd2578f3ee
arkud <artyomiius@gmail.com>
2020-05-05 00:28:35 +0300

    getPosts, removePosts, editPost added. Methods now log data. Added tests for all methods.

--- task6/script.js
+++ task6/script.js
@@ -7,7 +7,7 @@
             author: "ar_kud",
             name: "Artsiom Kudryavtsev",
             photoLink: "zZuTNe0PW34.jpg",
-            hashtags: ["NeHochuSessiu"],
+            hashtags: ["#NeHochuSessiu"],
             likes: ["ar_kud"]
         },
         {
@@ -17,7 +17,7 @@
             author: "ar_kud",
             name: "Artsiom Kudryavtsev",
             photoLink: "zZuTNe0PW34.jpg",
-            hashtags: [],
+            hashtags: ["#NeHochuSessiu"],
             likes: ["ar_kud"]
         },
         {
@@ -201,38 +201,63 @@
             likes: ["ar_kud"]
         }
     ];
-    alert("Im working");
+    var deletedPosts = [];
 
     function getPosts(skip, top, filterConfig) {
         skip = skip || 0;
         top = top || 10;
 
+        var result = posts.slice();
+
+        if (filterConfig.hashtags !== undefined && filterConfig.hashtags !== null) {
+            result = posts.filter(function (item) {
+                if(item.hashtags === null || item.hashtags === undefined)
+                    return false;
+                for(let i = 0;i < filterConfig.hashtags.length;i++){
+                    if(item.hashtags.includes(filterConfig.hashtags[i]))
+                        return true;
+                }
+
+            });
+        }
+
+        if (filterConfig.timeSort) {
+            result = result.sort((a, b) => a.createdAt - b.createdAt);
+        } else if (filterConfig.nameSort) {
+            result = result.sort((a, b) => a.name > b.name);
+        }
+
+        console.log("get posts, skipping " + skip + ", getting " + top + ", filtered by: "
+            + "name:" + filterConfig.nameSort + ", time:" + filterConfig.timeSort + ", with hashtags: " + (filterConfig.hashtags || "none") + "\n");
+        return result.slice(skip, skip + top);
     }
 
     function getPost(id) {
-        return posts.find(element => element.id === id);
+        let result = posts.find(element => element.id === id);
+        console.log("Find post with id:" + id + ", found: " + result + "\n");
+        return result;
     }
 
     function validatePost(post) {
+        console.log(post + " : ")
         if (post.id === undefined) {
+            console.log("wrong id\n");
             return false;
         }
         if (post.description === undefined || post.description.length >= 200) {
+            console.log("wrong description\n");
             return false;
         }
         if (post.createdAt === undefined || !(post.createdAt instanceof Date)) {
+            console.log("wrong time\n");
             return false;
         }
         if (post.author === undefined) {
+            console.log("wrong author\n");
             return false;
         }
         if (post.name === undefined) {
-            return false;
-        }
-        if (post.likes.filter((item, index) => post.likes.indexOf(item) !== index) === undefined) {
-            return false;
-        }
-        if (post.hashtags.filter((item, index) => post.hashtags.indexOf(item) !== index) === undefined) {
+            console.log("wrong name\n");
             return false;
         }
         return true;
@@ -240,13 +265,69 @@
 
     function addPost(post) {
         if (validatePost(post)) {
+            var re = /#.+ /;
+            post.hashtags = re.exec(post.description);
+            if (post.hashtags !== null)
+                post.hashtags = post.hashtags.filter((elem, index) => post.hashtags
+                    .indexOf(elem) === index);
             posts.push(post);
+            console.log("Succesfully added new post");
             return true;
         }
         return false;
     }
 
-    function editPost(is, post) {
+    function editPost(id, post) {
+        console.log("Change post with id:" + id);
+        if (post.createdAt !== undefined || post.author !== undefined || post.name !== undefined || post.id !== undefined) {
+            console.log("restricted fields cannot be changed");
+            return false;
+        }
+        if (!validatePost(getPost(id)))
+            return false;
 
+        var index = posts.findIndex(element => element.id === id);
+        if (post.description !== undefined) {
+            posts[index].description = post.description;
+            var re = /#.+ /;
+            posts[index].hashtags = re.exec(post.description);
+            if (posts[index].hashtags !== null)
+                posts[index].hashtags = posts[index].hashtags.filter((elem, index) => posts[index].hashtags
+                    .indexOf(elem) === index);
+            console.log("description succesfully changed to: " + post.description.slice(0, 20) + "...");
+            console.log("found hashtags " + posts[index].hashtags);
+        }
+
+        if (post.likes !== undefined) {
+            posts[index].likes = post.likes;
+            console.log("likes succesfully updated");
+        }
+        return true;
     }
+
+    function removePost(id) {
+        console.log("Remove post with id:" + id + ",");
+        var index = posts.findIndex(element => element.id === id);
+        if (index === -1) {
+            console.log(" not found\n");
+            return false;
+        }
+        deletedPosts.push(posts[index]);
+        posts.splice(index, 1);
+        console.log(" removed succesfully\n");
+        return true;
+    }
+
+
+    getPost("1");
+    removePost("1");
+    getPost("1");
+    addPost({id: "1", description: "zzz", createdAt: new Date(), author: "gtr", name: "You"});
+    getPost("1");
+    editPost("1", {description: "ddd"});
+    editPost("1", {createdAt: new Date()});
+    editPost("1", {likes: ["ar_kud"]});
+    console.log(getPosts(undefined, undefined, {timeSort: true})[0].createdAt);
+    console.log(getPosts(undefined, undefined, {nameSort: true})[0].name);
+    console.log(getPosts(undefined, undefined, {hashtags: ["#NeHochuSessiu"]})[0].hashtags[0]);
 }());
